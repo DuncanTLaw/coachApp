8b389a65fea3c78ec683446886d9cb31bf62c5cd
Duncan Law <lawdtk@gmail.com>
2021-01-27 22:52:45 +0000

    Initial Commit

---
+++ coachApp/coachAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  coachAppApp.swift
+//  coachApp
+//
+//  Created by Duncan Law on 27/1/2021.
+//
+
+import SwiftUI
+
+@main
+struct coachAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
